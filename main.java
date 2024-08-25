f86561f2f442132df8b21ae56185d3717e7dd787
Deepak9551 <12pcm.deepak@gmail.com>
2024-08-25 19:00:43 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,9 @@
+package object_class;
+
+public class main {
+    public static void main(String[] args) {
+        test t1 = new test();
+        t1.driver();
+        System.out.println("commit");
+    }
+}
